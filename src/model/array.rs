076a005dff663e3b84232a6cf0fb736ffb752a45
Krystian Muchla <kmuchla69@gmail.com>
2023-07-28 22:38:12 +0200

    Implement index trait for array

--- src/model/array.rs
+++ src/model/array.rs
@@ -1,3 +1,5 @@
+use std::ops::Index;
+
 #[derive(Clone)]
 pub struct Array<T> {
     dimensions: Vec<usize>,
@@ -13,31 +15,61 @@ impl<T: Clone> Array<T> {
     }
 
     pub fn of(dimensions: Vec<usize>, data: Vec<T>) -> Array<T> {
-        if data.len() != Self::get_dimensions_size(&dimensions) {
-            panic!("Data size does not match dimensions size.")
+        if data.len() != Self::get_data_len(&dimensions) {
+            panic!("Incorrect dimensions for provided data.")
         }
-        Array {
-            dimensions,
-            data,
-        }
+        Array { dimensions, data }
     }
 
     pub fn fill(dimensions: Vec<usize>, value: T) -> Array<T> {
         Array {
-            data: vec![value; Self::get_dimensions_size(&dimensions)],
+            data: vec![value; Self::get_data_len(&dimensions)],
             dimensions,
         }
     }
 
-    fn get_dimensions_size(dimensions: &Vec<usize>) -> usize {
+    fn get_data_len(dimensions: &Vec<usize>) -> usize {
         if dimensions.is_empty() {
             return 0;
         }
-        let mut dimensions_size: usize = 1;
+        let mut data_len: usize = 1;
         for dimension in dimensions {
-            dimensions_size *= dimension;
+            data_len *= dimension;
         }
-        dimensions_size
+        data_len
+    }
+}
+
+impl<T> Index<Vec<usize>> for Array<T> {
+    type Output = T;
+
+    fn index(&self, array_index: Vec<usize>) -> &T {
+        if array_index.is_empty() {
+            panic!("Array index cannot be empty.")
+        }
+        let dimensions = &self.dimensions;
+        if array_index.len() != dimensions.len() {
+            panic!(
+                "Array index length {} does not match dimensions length {}.",
+                array_index.len(),
+                dimensions.len()
+            )
+        }
+        let mut data_index: usize = 0;
+        let mut offset: usize = 1;
+        for dimensions_index in 0..dimensions.len() {
+            let dimension = dimensions[dimensions_index];
+            let dimension_index = array_index[dimensions_index];
+            if dimension_index >= dimension {
+                panic!(
+                    "Provided dimension index {} is out of bounds for dimension of length {}.",
+                    dimension_index, dimension
+                )
+            }
+            data_index += dimension_index * offset;
+            offset *= dimension;
+        }
+        return &self.data[data_index];
     }
 }
 
@@ -55,7 +87,7 @@ mod tests {
 
     #[test]
     fn should_create_array_of() {
-        let dimensions: Vec<usize> = vec![2, 3, 2];
+        let dimensions = vec![2, 3, 2];
         let data: Vec<isize> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
         let array: Array<isize> = Array::of(dimensions, data);
@@ -67,7 +99,7 @@ mod tests {
     #[test]
     #[should_panic]
     fn should_not_create_array_of_larger_dimensions_than_data() {
-        let dimensions: Vec<usize> = vec![1, 2, 3];
+        let dimensions = vec![1, 2, 3];
         let data: Vec<isize> = vec![1, 1];
 
         Array::of(dimensions, data);
@@ -76,7 +108,7 @@ mod tests {
     #[test]
     #[should_panic]
     fn should_not_create_array_of_larger_data_than_dimensions() {
-        let dimensions: Vec<usize> = vec![1, 1];
+        let dimensions = vec![1, 1];
         let data: Vec<isize> = vec![1, 2];
 
         Array::of(dimensions, data);
@@ -84,12 +116,55 @@ mod tests {
 
     #[test]
     fn should_fill_array() {
-        let dimensions: Vec<usize> = vec![1, 2, 3];
+        let dimensions = vec![1, 2, 3];
         let value: isize = 11;
 
         let array: Array<isize> = Array::fill(dimensions, value);
 
         assert_eq!(array.dimensions, vec![1, 2, 3]);
         assert_eq!(array.data, vec![11, 11, 11, 11, 11, 11]);
+    }
+
+    #[test]
+    fn should_get_array_element_by_index() {
+        let array_index = vec![0, 2, 1];
+        let dimensions = vec![2, 3, 2];
+        let data: Vec<isize> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+        let array: Array<isize> = Array::of(dimensions, data);
+
+        let array_element = array[array_index];
+
+        assert_eq!(array_element, 11);
+    }
+
+    #[test]
+    #[should_panic]
+    fn should_not_get_array_element_by_index_when_empty() {
+        let array_index = vec![];
+        let array: Array<isize> = Array::new();
+
+        array[array_index];
+    }
+
+    #[test]
+    #[should_panic]
+    fn should_not_get_array_element_by_index_with_different_dimensions_length() {
+        let array_index = vec![0, 0, 0];
+        let dimensions = vec![2, 2];
+        let data: Vec<isize> = vec![1, 2, 3, 4];
+        let array: Array<isize> = Array::of(dimensions, data);
+
+        array[array_index];
+    }
+
+    #[test]
+    #[should_panic]
+    fn should_not_get_array_element_by_index_with_invalid_dimension_index() {
+        let array_index = vec![0, 999];
+        let dimensions = vec![2, 2];
+        let data: Vec<isize> = vec![1, 2, 3, 4];
+        let array: Array<isize> = Array::of(dimensions, data);
+
+        array[array_index];
     }
 }
